28b43638bc71ee106895511ab82ead2909a217d3
Mariano Franceschetti <mariano.f@iomico.com>
2026-01-07 09:19:22 -0300

    [ID-274] Fixed pin assignment missmatch

--- src/main.cpp
+++ src/main.cpp
@@ -112,17 +112,17 @@ int main(void) {
     while (1) {
         ret = gpio_pin_toggle_dt(&led);
         // Fetch fresh data
-        // sensor_sample_fetch(dev);
+        sensor_sample_fetch(dev);
 
-        // struct sensor_value temp, press, humidity, gas;
-        // sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp);
-        // sensor_channel_get(dev, SENSOR_CHAN_PRESS, &press);
-        // sensor_channel_get(dev, SENSOR_CHAN_HUMIDITY, &humidity);
-        // sensor_channel_get(dev, SENSOR_CHAN_GAS_RES, &gas);
+        struct sensor_value temp, press, humidity, gas;
+        sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+        sensor_channel_get(dev, SENSOR_CHAN_PRESS, &press);
+        sensor_channel_get(dev, SENSOR_CHAN_HUMIDITY, &humidity);
+        sensor_channel_get(dev, SENSOR_CHAN_GAS_RES, &gas);
 
-        // LOG_INF("BME680 readings\n\tT: %d.%06d; P: %d.%06d; H: %d.%06d; G: %d.%06d",
-        //         temp.val1, temp.val2, press.val1, press.val2,
-        //         humidity.val1, humidity.val2, gas.val1, gas.val2);
+        LOG_INF("BME680 readings\n\tT: %d.%06d; P: %d.%06d; H: %d.%06d; G: %d.%06d",
+                temp.val1, temp.val2, press.val1, press.val2,
+                humidity.val1, humidity.val2, gas.val1, gas.val2);
         LOG_INF("LED toggled.");
 
         switch (buttonPushed) {
